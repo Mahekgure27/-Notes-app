694ff134fb695b080ba496e401445e2d04f69096
Mahek gure <mahekgure05@gmail.com>
2026-02-05 15:25:40 +0530

    Ui improved

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import axios from "axios"
 // import noteModel from '../../../Backend/src/models/notes.models';
@@ -8,28 +8,82 @@ import axios from "axios"
 function App() {
   const [notes , setNote] = useState([])
 
-  axios
-  .get("http://localhost:3000/notes")
-  .then((res)=>{
+  function fetchNotes(){
+    axios
+    .get("http://localhost:3000/notes")
+    .then((res)=>{
     setNote(res.data.notes);
-  })
+    })
+  }
+
+  useEffect(()=>{
+    fetchNotes();
+  },[])
+ 
+  function handleSubmit(e){
+    e.preventDefault()
+
+    const {title, description} = e.target.elements
+
+    console.log(title.value, description.value);
+
+    axios.post("http://localhost:3000/notes",{
+      title:title.value,
+      description:description.value
+    })
+    .then(res=>{
+      console.log(res.data)
+      
+      fetchNotes();
+    })
+  }
+
+  function handleDeleteNote(noteId){
+    axios.delete("http://localhost:3000/notes"+noteId)
+    .then(res=>{
+      console.log(res.data);
+    })
+  }
+
   return (
     <>
       {/* <div className="container">
         <h1>Title</h1>
         <p>Description</p>
       </div> */}
+   <form onSubmit={handleSubmit}>
+  
+  <input
+    type="text"
+    name="title"
+    placeholder="Enter title"
+  />
 
-   {
-    notes.map((note,index)=>{
-      return (
-      <div className="container">
+  <input
+    type="text"
+    name="description"
+    placeholder="Enter description"
+  />
+  <button type="submit">Add</button>
+</form>
+
+    <div className="dashboard">
+ {
+  notes.map((note, index) => {
+    return (
+      <div className="container" key={index}>
         <h1>{note.title}</h1>
         <p>{note.description}</p>
+
+        <div className="actions">
+          <button className="update-btn">Update</button>
+          <button className="delete-btn" onClick={()=>{handleDeleteNote(note._id)}}>Delete</button>
+        </div>
       </div>
-      )
-    })
-   }
+    );
+  })
+}
+  </div>
     </>
   )
 }
