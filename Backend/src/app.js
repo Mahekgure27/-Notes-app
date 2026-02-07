f183570e8566cd6d93691129477f5ff2f451eef2
Mahekgure27 <mahek8796@gmail.com>
2026-02-07 11:42:36 +0530

    frontend integrated successfully

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,19 +6,7 @@ const path = require("path")
 const app = express()
 app.use(cors())
 app.use(express.json())
-
-// app.post("/notes",async (req,res)=>{
-//     const {title,description} = req.body
-
-//     const note = await noteModel.create({
-//         title, description
-//     })
-
-//     res.status(201).json({
-//         message:"note created successfully",
-//         note
-//     })
-// })
+app.use(express.static("./public"))
 
 app.post("/notes",async (req,res)=>{
     const {title,description} = req.body
