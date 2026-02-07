034303eff0006143456d463d28d8ec9e8d023283
Mahekgure27 <mahek8796@gmail.com>
2026-02-07 11:56:21 +0530

    dotenv added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,8 @@ const noteModel = require("./models/notes.models")
 const cors = require("cors")
 const path = require("path")
 
+require("dotenv").config();
+
 const app = express()
 app.use(cors())
 app.use(express.json())
